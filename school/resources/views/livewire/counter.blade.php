8a08d840770babea69ed12dabed85bd2be24ced8
InJoon-L <dlswns619@naver.com>
2021-09-01 16:05:28 +0900

    livewire init

---
+++ school/resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    {{-- <h1>안녕하세요? livewire 컴포넌트 입니다.</h1> --}}
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <h1>{{ $count }}</h1>
+        <button wire:click="decrement">-</button>
+    </div>
+</div>
---
+++ study-app/routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\JWTAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::get('user', [JWTAuthController::class, 'user']);
+    Route::post('register', [JWTAuthController::class, 'register']);
+    Route::post('login', [JWTAuthController::class, 'login']);
+    Route::get('logout', [JWTAuthController::class, 'logout']);
+    Route::get('refresh', [JWTAuthController::class, 'refresh']);
+});
+
+Route::get('unauthorized', function() {
+    return response()->json([
+        'status' => 'error',
+        'message' => 'Unauthorized'
+    ], 401);
+})->name('api.jwt.unauthorized');
